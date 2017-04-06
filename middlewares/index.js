87c75fc3e15240ea6ffb31d7a6fe3a64fae4cedd
Alexandre Morin <alexandre.morin@maarch.org>
2017-04-06 22:17:45 +0200

    First commit

---
+++ middlewares/index.js
@@ -0,0 +1,11 @@
+module.exports = (api) => {
+    api.middlewares = {
+        bodyParser: require('body-parser'),
+        logger: require('./logger'),
+        res: require('./res'),
+        isAuthenticated: require('./isAuthenticated')(api),
+        ensureFields: require('./ensureFields'),
+        acl: require('./acl'),
+        cache: require('./cache')
+    };
+};
