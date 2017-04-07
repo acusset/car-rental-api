18ca8791f3c7099c95251d460a774ee18a897f3c
Jolan Levy <jolan.levy@gmail.com>
2017-04-07 11:56:11 +0200

    Add CRUD agencies

---
+++ actions/agencies.js
@@ -0,0 +1,42 @@
+module.exports = (api) => {
+    const Agency = api.models.Agency;
+
+    function create(req, res, next) {
+        let agency = new Agency(req.body);
+        agency.save()
+            .then(res.prepare(201))
+            .catch(res.prepare(500));
+    }
+
+    function list(req, res, next) {
+        Agency.find()
+            .then(res.prepare(200))
+            .then(res.prepare(500));
+    }
+
+    function show(req, res, next) {
+        Agency.findById(req.params.id)
+            .then(res.prepare(200))
+            .catch(res.prepare(500));
+    }
+
+    function update(req, res, next) {
+        Agency.findByIdAndUpdate(req.params.id, req.body)
+            .then(res.prepare(204))
+            .catch(res.prepare(500));
+    }
+
+    function remove(req, res, next) {
+        Agency.findByIdAndRemove(req.params.id)
+            .then(res.prepare(204))
+            .catch(res.prepare(500));
+    }
+
+    return {
+        create,
+        list,
+        show,
+        update,
+        remove
+    };
+};
