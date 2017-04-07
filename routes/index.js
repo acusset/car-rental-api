399dde6a8a394ac2b6f4a83bac129537e661b316
Antoine Cusset <acusset@clever-age.com>
2017-04-07 11:35:46 +0200

    add rentings routes

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,5 @@ module.exports = (api) => {
     api.use('/auth', require('./auth')(api));
     api.use('/models', require('./models')(api));
     api.use('/agencies', require('./agencies')(api));
-
+    api.use('/rentings', require('./rentings')(api));
 };
---
+++ routes/rentings.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+module.exports = (api) => {
+    /**
+     * List all rentings
+     * Require Auth + Admin
+     */
+    router.get('/',
+        api.middlewares.isAuthenticated,
+        api.middlewares.acl.ensure(1),
+        api.actions.rentings.list
+    );
+
+
+
+};
