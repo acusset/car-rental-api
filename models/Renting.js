5729cdfda468150285179ef903cdb34e99c99a6e
1992clement <cltrumpff@gmail.com>
2017-04-07 11:19:00 +0200

    ajout des models

---
+++ models/Renting.js
@@ -0,0 +1,33 @@
+module.exports = (api) => {
+    const mongoose = api.mongoose;
+    const Schema = mongoose.Schema;
+
+    let RentingsSchema = Schema({
+        rentingPoint: {
+            type: Schema.Types.ObjectId,
+            ref: 'Agency',
+        },
+        backPoint: {
+            type: Schema.Types.ObjectId,
+            ref: 'Agency',
+        },
+        rentingDate: {
+            type: String,
+            required: true
+        },
+        backDate: {
+            type: Object,
+            required: true
+        },
+        carId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Car',
+        },
+        usersId: [{
+            type: Number,
+            required: true,
+        }]
+    });
+
+    return mongoose.model('Rentings', RentingsSchema);
+};
