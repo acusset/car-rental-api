90664f7688ee9529574f4f76cd4326031540f789
1992clement <cltrumpff@gmail.com>
2017-04-07 11:56:10 +0200

    CRUD rentings

--- actions/index.js
+++ actions/index.js
@@ -2,6 +2,7 @@ module.exports = (api) => {
     api.actions = {
         users: require('./users')(api),
         cars: require('./cars')(api),
-        auth: require('./auth')(api)
+        auth: require('./auth')(api),
+        rentings: require('./rentings')(api)
     };
 };
